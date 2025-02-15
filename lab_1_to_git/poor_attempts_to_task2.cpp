adb763d107e8ae88b21fae17cf9ca4c878731fc1
bisordesolidus <bisordesolidus@gmail.com>
2025-02-15 09:47:37 +0300

    fixet poor atte,pts

--- lab_1_to_git/poor_attempts_to_task2.cpp
+++ lab_1_to_git/poor_attempts_to_task2.cpp
@@ -1,4 +1,123 @@
+// -----------------------------------------------------------------------------
+//
+//  Gmsh C++ tutorial 13
+//
+//  Remeshing an STL file without an underlying CAD model
+//
+// -----------------------------------------------------------------------------
+
 #include <set>
+#include <cmath>
+#include <gmsh.h>
+
+int main(int argc, char **argv)
+{
+  gmsh::initialize();
+
+  gmsh::model::add("t13");
+
+  // Let's merge an STL mesh that we would like to remesh (from the parent
+  // directory):
+  try {
+    gmsh::merge("../tarakan.STL");
+  } catch(...) {
+    gmsh::logger::write("Could not load STL mesh: bye!");
+    gmsh::finalize();
+    return 0;
+  }
+
+  // We first classify ("color") the surfaces by splitting the original surface
+  // along sharp geometrical features. This will create new discrete surfaces,
+  // curves and points.
+
+
+  // Angle between two triangles above which an edge is considered as sharp:
+  double angle = 40;
+
+  // For complex geometries, patches can be too complex, too elongated or too
+  // large to be parametrized; setting the following option will force the
+  // creation of patches that are amenable to reparametrization:
+  bool forceParametrizablePatches = true;
+
+  // For open surfaces include the boundary edges in the classification process:
+  bool includeBoundary = true;
+
+  // Force curves to be split on given angle:
+  double curveAngle = 180;
+
+  gmsh::model::mesh::classifySurfaces(angle * M_PI / 180., includeBoundary,
+                                      forceParametrizablePatches,
+                                      curveAngle * M_PI / 180.);
+
+  // Create a geometry for all the discrete curves and surfaces in the mesh, by
+  // computing a parametrization for each one
+  gmsh::model::mesh::createGeometry();
+
+  // Create a volume from all the surfaces
+  
+  double mesh_size = 0.4;
+  gmsh::option::setNumber("Mesh.MeshSizeMax", mesh_size);
+
+  std::vector<std::pair<int, int> > s;
+  gmsh::model::getEntities(s, 2);
+  gmsh::model::mesh::setSize(s, mesh_size);
+  std::vector<int> sl;
+  for(auto surf : s) sl.push_back(surf.second);
+  int l = gmsh::model::geo::addSurfaceLoop(sl);
+  gmsh::model::geo::addVolume({l});
+
+  gmsh::model::geo::synchronize();
+
+  // We specify element sizes imposed by a size field, just because we can :-)
+  bool funny = false; // false;
+  int f = gmsh::model::mesh::field::add("MathEval");
+  if(funny)
+    gmsh::model::mesh::field::setString(f, "F", "2*Sin((x+y)/5) + 3");
+  else
+    gmsh::model::mesh::field::setString(f, "F", "4");
+  gmsh::model::mesh::field::setAsBackgroundMesh(f);
+
+  gmsh::model::mesh::generate(3);
+
+  gmsh::write("hand.msh");
+
+  // Launch the GUI to see the results:
+  std::set<std::string> args(argv, argv + argc);
+  if(!args.count("-nopopup")) gmsh::fltk::run();
+
+  
+  gmsh::finalize();
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*#include <set>
 #include <cmath>
 #include <gmsh.h>
 #include <iostream>
@@ -8,9 +127,10 @@ int main(int argc, char** argv) {
     gmsh::initialize();
     gmsh::option::setNumber("General.Terminal", 1);
     gmsh::model::add("iron_man");
-    gmsh::option::setNumber("Mesh.CharacteristicLengthMin", 0.1);
-    gmsh::option::setNumber("Mesh.CharacteristicLengthMax", 0.4);
-    std::string stl_file = "../iron_man.stl";
+    //gmsh::option::setNumber("Mesh.CharacteristicLengthMin", 0.1);
+    //gmsh::option::setNumber("Mesh.CharacteristicLengthMax", 0.4);
+    gmsh::option::setNumber("Mesh.MeshSizeMax", 1);
+    std::string stl_file = "../merge_file.stl";
     try {
         gmsh::merge(stl_file);
     } catch (const std::exception& e) {
@@ -24,11 +144,11 @@ int main(int argc, char** argv) {
     //gmsh::option::setNumber("Mesh.AngleToleranceFacetOverlap", 5.0);
     //gmsh::option::setNumber("Mesh.Tolerance", 0.01);
     // Создаем топологию
-  gmsh::model::mesh::createTopology();
+  //gmsh::model::mesh::createTopology();
     //gmsh::model::mesh::createGeometry();
-  double angle = 40;
+  double angle = 60;
 
-  bool forceParametrizablePatches = false;
+  bool forceParametrizablePatches = true;
 
   bool includeBoundary = true;
   double curveAngle = 180;
@@ -53,7 +173,7 @@ int main(int argc, char** argv) {
 
     gmsh::finalize();
     return 0;
-}
+}*/
     /*gmsh::initialize();
     gmsh::option::setNumber("General.Terminal", 1);
 
